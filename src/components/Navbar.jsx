e5c7f227c755f5a7178c8cac7c7a18820f06bf22
Manuel Leguizamon <manulegui08@gmail.com>
2024-07-11 12:00:05 -0300

    arreglos

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return(
+        <>
+        <ul>
+            <li>Inicio</li>
+            <li>Info</li>
+            <li>Contacto</li>
+        </ul>
+        </>
+    )
+}
+
+export default Navbar
